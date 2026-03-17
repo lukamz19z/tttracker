f6f26dd1c57a459cb0f226e03e7a4bd86eb9fc14
Luka Z <lmzetovic@gmail.com>
2026-03-17 11:09:13 +1100

    Fix supabase browser client

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "@/lib/supabase";
+import { createSupabaseBrowser } from "@/lib/supabase";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   const router = useRouter();
+  const supabase = createSupabaseBrowser();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
