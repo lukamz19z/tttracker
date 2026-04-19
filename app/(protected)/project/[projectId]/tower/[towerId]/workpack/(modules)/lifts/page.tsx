714ed129a2e25b88e3f88b257c2590c57dfc967f
lukamz19z <lmzetovic@gmail.com>
2026-04-19 20:36:20 +1000

    d

--- app/(protected)/project/[projectId]/tower/[towerId]/workpack/(modules)/lifts/page.tsx
+++ app/(protected)/project/[projectId]/tower/[towerId]/workpack/(modules)/lifts/page.tsx
@@ -342,7 +342,12 @@ export default function LiftStudiesPage() {
         >
           Lift Studies
         </Link>
-
+<Link
+  className="px-4 py-2 bg-slate-100 border rounded-t-lg whitespace-nowrap"
+  href={`/project/${projectId}/tower/${towerId}/workpack/drawings`}
+>
+  Drawings
+</Link>
         <Link
           className="px-4 py-2 bg-slate-100 border rounded-t-lg whitespace-nowrap"
           href={`/project/${projectId}/tower/${towerId}/workpack/documents`}
--- app/(protected)/project/[projectId]/tower/[towerId]/workpack/(modules)/permits/page.tsx
+++ app/(protected)/project/[projectId]/tower/[towerId]/workpack/(modules)/permits/page.tsx
@@ -495,7 +495,12 @@ export default function PermitsPage() {
         >
           Lift Studies
         </Link>
-
+<Link
+  className="px-4 py-2 bg-slate-100 border rounded-t-lg whitespace-nowrap"
+  href={`/project/${projectId}/tower/${towerId}/workpack/drawings`}
+>
+  Drawings
+</Link>
         <Link
           className="px-4 py-2 bg-slate-100 border rounded-t-lg whitespace-nowrap"
           href={`/project/${projectId}/tower/${towerId}/workpack/documents`}
